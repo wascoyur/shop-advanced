2da141df0ff8ff299c147fc6acc3b9602de232eb
wascoyur <mytem20007@gmail.com>
2021-03-22 23:30:24 +0500

    routs and pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Home from "./pages/Home";
+import {Switch, Route} from 'react-router-dom'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Switch>
+      <Route path='/' component={Home} exact/>
+      <Route path="/login" component={Login} exact />
+      <Route path='/register' component={Register} exact/>
+    </Switch>
+  )
 }
 
 export default App;
