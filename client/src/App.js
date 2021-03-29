739c0d1bde59407aa4618074595f4d0ace50d799
wascoyur-swetBook <mytem20007@gmail.com>
2021-03-29 15:18:02 +0500

    2.18.User in Redux State 11:00

--- client/src/App.js
+++ client/src/App.js
@@ -7,9 +7,27 @@ import { Fragment } from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import RegisterComplete from './pages/auth/RegisterCoplete';
-
+import { auth } from './firebase';
+import { useDispatch } from 'react-redux';
+import {useEffect} from 'react'
 
 const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        const idTokenResult = await user.getIdTokenResult();
+        dispatch({
+          type: 'LOGGED_IN_USER',
+          payload: {
+            email: user.email,
+            token:idTokenResult.token
+          }
+        })
+      }
+    })
+    return ()=> unsubscribe()
+  }, []);
   return (
     <Fragment>
       <Header />
