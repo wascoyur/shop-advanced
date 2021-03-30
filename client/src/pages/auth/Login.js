90dbd03b2aaeb900c8399bf26e9e70ea1f682a27
wascoyur-swetBook <mytem20007@gmail.com>
2021-03-30 11:44:51 +0500

    2.20.Login Page 12:02

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -1,11 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { MailOutlined } from '@ant-design/icons';
+import { Button } from 'antd';
 
 const Login = () => {
-  return (
-    <div>
-      <p>Login</p>
-    </div>
-  )
-}
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-export default Login
+  const loginForm = () => (
+    <form onSubmit={handleSubmit}>
+      <div className='form-group'>
+        <input
+          type='email'
+          className='form-control'
+          value={email}
+          placeholder='Введите адрес электроной почты'
+          onChange={e => {
+            setEmail(e.target.value);
+          }}
+        />
+      </div>
+
+      <div className='form-group'>
+        <input
+          type='password'
+          className='form-control'
+          placeholder='Введите установленный пароль'
+          onChange={e => {
+            setPassword(e.target.value);
+          }}
+        />
+      </div>
+
+      <Button
+        type='primary'
+        className='mb-3'
+        icon={<MailOutlined />}
+        onClick={handleSubmit}
+        disabled={!email || !password.length <6}
+      >
+        Войти с логином и паролем
+      </Button>
+    </form>
+  );
+  const handleSubmit = async e => {
+    e.preventDefault();
+    console.table(email, password);
+  };
+  return (
+    <div className='container p-5'>
+      <div className='row'>
+        <div className='col-md-6 offset-md-3'>
+          <h4>Вход</h4>
+          {loginForm()}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
