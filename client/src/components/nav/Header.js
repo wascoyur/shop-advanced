fe7b0975f672324966a1b2590ae57fedbaef4942
wascoyur-swetBook <mytem20007@gmail.com>
2021-03-30 09:27:21 +0500

    2.19.User Logout 08:18

--- client/src/components/nav/Header.js
+++ client/src/components/nav/Header.js
@@ -5,20 +5,33 @@ import {
   UserOutlined,
   SettingOutlined,
   UserAddOutlined,
+  LogoutOutlined,
 } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
+import firebase from 'firebase/app';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom'
 
 const { SubMenu, Item } = Menu;
 const Header = () => {
-    const [current, setCurrent] = useState('home');
-    
+  const [current, setCurrent] = useState('home');
+  const dispatch = useDispatch();
+  let history = useHistory();
 
-  const handleClick = (e) => {
+  const logout = () => {
+    firebase.auth().signOut();
+    dispatch({
+      type: 'LOGOUT',
+      payload: null,
+    });
+    history.push('/login');
+  };
+
+  const handleClick = e => {
     setCurrent(e.key);
   };
 
-    return (
-      
+  return (
     <Menu onClick={handleClick} selectedKeys={[current]} mode='horizontal'>
       <Item key='home' icon={<HomeOutlined />}>
         <Link to='/'>Домой</Link>
@@ -26,15 +39,14 @@ const Header = () => {
       <SubMenu key='SubMenu' icon={<SettingOutlined />} title='Действия'>
         <Item key='point1'>Пункт 1 </Item>
         <Item key='point2'>Пункт 2 </Item>
+        <Item icon={<LogoutOutlined />} key='point3' onClick={logout}>
+          Выйти{' '}
+        </Item>
       </SubMenu>
       <Item key='signin' icon={<UserOutlined />} className='float-right'>
         <Link to='/login'>Войти с учетной записью</Link>
       </Item>
-      <Item
-        key='signup'
-        icon={<UserAddOutlined />}
-        className='float-right'
-      >
+      <Item key='signup' icon={<UserAddOutlined />} className='float-right'>
         <Link to='/register'>Зарегистрироваться</Link>
       </Item>
     </Menu>
