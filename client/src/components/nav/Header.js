5affc61088a3853cefec9464339e7bec80c8f644
wascoyur-swetBook <mytem20007@gmail.com>
2021-03-31 13:26:08 +0500

    2.23.Conditional Rendering 09:17

--- client/src/components/nav/Header.js
+++ client/src/components/nav/Header.js
@@ -9,14 +9,15 @@ import {
 } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
 import firebase from 'firebase/app';
-import { useDispatch } from 'react-redux';
-import { useHistory } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 const { SubMenu, Item } = Menu;
 const Header = () => {
   const [current, setCurrent] = useState('home');
   const dispatch = useDispatch();
   let history = useHistory();
+  let { user } = useSelector(state => ({ ...state }));
 
   const logout = () => {
     firebase.auth().signOut();
@@ -36,19 +37,26 @@ const Header = () => {
       <Item key='home' icon={<HomeOutlined />}>
         <Link to='/'>Домой</Link>
       </Item>
-      <SubMenu key='SubMenu' icon={<SettingOutlined />} title='Действия'>
-        <Item key='point1'>Пункт 1 </Item>
-        <Item key='point2'>Пункт 2 </Item>
-        <Item icon={<LogoutOutlined />} key='point3' onClick={logout}>
-          Выйти{' '}
+      {user && (
+        <SubMenu key='SubMenu' icon={<SettingOutlined />} title={user.email.split('@')[0]} className='float-right'>
+          <Item key='point1'>Пункт 1 </Item>
+          <Item key='point2'>Пункт 2 </Item>
+          <Item icon={<LogoutOutlined />} key='point3' onClick={logout}>
+            Выйти{' '}
+          </Item>
+        </SubMenu>
+      )}
+
+      {!user && (
+        <Item key='signup' icon={<UserAddOutlined />} className='float-right'>
+          <Link to='/register'>Зарегистрироваться</Link>
         </Item>
-      </SubMenu>
-      <Item key='signin' icon={<UserOutlined />} className='float-right'>
-        <Link to='/login'>Войти с учетной записью</Link>
-      </Item>
-      <Item key='signup' icon={<UserAddOutlined />} className='float-right'>
-        <Link to='/register'>Зарегистрироваться</Link>
-      </Item>
+      )}
+      {!user && (
+        <Item key='signin' icon={<UserOutlined />} className='float-right'>
+          <Link to='/login'>Войти с учетной записью</Link>
+        </Item>
+      )}
     </Menu>
   );
 };
