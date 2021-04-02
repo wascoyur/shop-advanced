1231f471f64d85a31ee7e56d03e4d59f3dc5b0fb
wascoyur-swetBook <mytem20007@gmail.com>
2021-04-02 12:57:38 +0500

    add /server

---
+++ server/configdb/configdb.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// console.log('dbjs-process.env.MONGO_URI', process.env.MONGO_URI);
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.DATABASE, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    });
+    console.log(`Connect to Db: ${conn.connection.host}`.blue.bold);
+  } catch (e) {
+    console.log(`Error: ${e.message}`.redbold);
+    process.exit(1);
+  }
+};
+export default connectDB;
---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require('dotenv').config();
+
+const app = express();
+
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+  })
+  .then(() => console.log('DB connect'))
+  .catch(err => console.log('error connection', err));
+
+app.use(morgan('dev'));
+app.use(bodyParser.json({ limit: '2mb' }));
+app.use(cors());
+
+app.get('/api', (req, res) => {
+  res.json({
+    data: 'hit API',
+  });
+});
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`Server run on port: ${PORT}`));
