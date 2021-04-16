c10210abeab68ea0f2f15a8c56e4af3485b0e703
wascoyur-xub <mytem20007@gmail.com>
2021-04-16 22:26:07 +0500

    Firebase Admin 05:36

---
+++ server/firebase/index.js
@@ -0,0 +1,8 @@
+var admin = require('firebase-admin');
+
+var serviceAccount = require('../configdb/shop-advanced-key.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://shop-advanced-c6026.iam.gserviceaccount.com',
+});
