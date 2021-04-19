b7be188b9c7c862c1df64c6eaee974fca7113d46
wascoyur-xub <mytem20007@gmail.com>
2021-04-19 10:57:13 +0500

    35.Middleware 09:04

--- server/firebase/index.js
+++ server/firebase/index.js
@@ -4,5 +4,6 @@ var serviceAccount = require('../configdb/shop-advanced-key.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://shop-advanced-c6026.iam.gserviceaccount.com',
 });
+
+module.exports = admin
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const router = express.Router()
-const {CreateOrUpdateUser} = require('../controllers/auth')
+const router = express.Router();
 
-router.get('/crateupdate', CreateOrUpdateUser);
+const { CreateOrUpdateUser } = require('../controllers/auth');
+//middleware
+const { authCheck } = require('../middlewares/middlewareAuth');
+
+router.post('/crateupdate', authCheck, CreateOrUpdateUser);
 
 module.exports = router;
- 
