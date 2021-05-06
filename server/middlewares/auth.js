9841160a978ccd7f6c9f0d6ec0578389ee5fbeb2
wascoyur-xub <mytem20007@gmail.com>
2021-05-06 09:58:41 +0500

    fix: middleware User

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,4 +1,5 @@
 const admin = require('../firebase');
+const User = require('../models/user');
 
 exports.authCheck = async (req, res, next) => {
   // console.log(req.headers); // token
@@ -17,6 +18,8 @@ exports.authCheck = async (req, res, next) => {
 };
 
 exports.adminCheck = async (req, res, next) => {
+  // console.log('request:', req.user);
+
   const { email } = req.user;
   const adminUser = await User.findOne({ email }).exec();
   if (adminUser !== 'admin') {
