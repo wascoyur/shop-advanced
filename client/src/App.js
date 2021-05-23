a019d53ea33a296712813e444db451d05c7024c7
wascoyur-xub <mytem20007@gmail.com>
2021-05-23 08:53:07 +0500

    64.Sub Categories Functions 06:15

--- client/src/App.js
+++ client/src/App.js
@@ -20,6 +20,7 @@ import AdminRoute from './components/routes/AdminRoute';
 import AdminDashboard from './pages/admin/AdminDashboard';
 import CategoryCreate from './pages/admin/category/CategoryCreate';
 import CategoryUpdate from './pages/admin/category/CategoryUpdate';
+import SubCreate from './pages/admin/sub/SubCreate';
 
 const App = () => {
   const dispatch = useDispatch();
@@ -68,6 +69,7 @@ const App = () => {
         <UserRoute exact path='/user/wishlist' component={Wishlist} />
         <AdminRoute exact path='/admin/dashboard' component={AdminDashboard} />
         <AdminRoute exact path='/admin/category' component={CategoryCreate} />
+        <AdminRoute exact path='/admin/sub' component={SubCreate} />
         <AdminRoute
           exact
           path='/admin/category/:slug'
