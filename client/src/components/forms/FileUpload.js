ff4a4c1c9c5c6037f72b9b58d97f1ce66c69c494
wascoyur-xub <mytem20007@gmail.com>
2021-06-08 23:15:59 +0500

    87.File Resize in React 11:41

--- client/src/components/forms/FileUpload.js
+++ client/src/components/forms/FileUpload.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import Resizer from 'react-image-file-resizer';
+import { useSelector } from 'react-redux';
+import axios from 'axios';
 
 const FileUpload = () => {
+  const { user } = useSelector((state) => ({ ...state }));
   const fileUploadAndResize = (e) => {
     // console.log(e.target.files);
+    let files = e.target.files;
+    if (files) {
+      for (let i = 0; i < files.length; i++) {
+        Resizer.imageFileResizer(
+          files[i],
+          720,
+          720,
+          'JPEG',
+          100,
+          0,
+          (uri) => {
+            console.log('uri', uri);
+          },
+          'base64',
+        );
+      }
+    }
   };
   return (
     <div className='row'>
