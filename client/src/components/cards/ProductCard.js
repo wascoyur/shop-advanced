7ba7af2e190a0b6dbe76880ba5fde168bd0fd224
wascoyur-xub <mytem20007@gmail.com>
2021-06-17 18:08:05 +0500

    110.Product Card Component 08:20

---
+++ client/src/components/cards/ProductCard.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Card } from 'antd';
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
+import { Row } from 'antd';
+
+const { Meta } = Card;
+
+const ProductCard = ({ product }) => {
+  return (
+    <div>{product.title}</div>
+    // <Card
+    //   cover={
+    //     <img
+    //       className='p-1'
+    //       alt=''
+    //       src={images[0].url}
+    //       style={{ hight: '150px', objectFit: 'cover' }}
+    //     />
+    //   }
+    //   actions={[
+    //     <DeleteOutlined
+    //       key='delete'
+    //       className='text-warning'
+    //       onClick={() => {}}
+    //     />,
+    //     <Link to={`/admin/product/${_id}`}>
+    //       <EditOutlined key='edit' className='text-danger' />
+    //     </Link>,
+    //   ]}>
+    //   <Meta
+    //     title={title}
+    //     description={`${description && description.substring(0, 10)}...`}
+    //   />
+    // </Card>
+  );
+};
+
+export default ProductCard;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { getProductsByCount } from '../functions/product';
+import ProductCard from '../components/cards/ProductCard';
 
 const Home = () => {
   const [products, setProducts] = useState([]);
@@ -10,15 +11,26 @@ const Home = () => {
   }, []);
 
   const loadAllProducts = () => {
+    setLoading(true);
     getProductsByCount(3).then((res) => {
       setProducts(res.data);
     });
+    setLoading(false);
   };
 
   return (
     <div>
-      <p>react home</p>
-      {JSON.stringify(products)}
+      <div className='jumbotron'>
+        {loading ? <h4>Загружается...</h4> : <p>Все товары</p>}
+        {/* {JSON.stringify(products)} */}{' '}
+      </div>
+      <div className='row'>
+        {products.map((product) => (
+          <div className='col-md-4' key={product._id}>
+            <ProductCard product={product} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
