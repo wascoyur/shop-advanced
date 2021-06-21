35608a6648256bd05782d5995ab44ab5b7ad674c
wascoyur-xub <mytem20007@gmail.com>
2021-06-21 17:38:13 +0500

    123.Image Carousel 07:44

--- client/src/components/cards/SingleProduct.js
+++ client/src/components/cards/SingleProduct.js
@@ -2,26 +2,23 @@ import React, { Fragment } from 'react';
 import { Card } from 'antd';
 import { Link } from 'react-router-dom';
 import { HeartOutlined, ShoppingCartOutlined } from '@ant-design/icons';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 const { Meta } = Card;
 
 const SingleProduct = ({ product }) => {
   const { title, description, images, slug } = product;
-  // const image=images[0]
+
   return (
     <Fragment>
-      <div className='col-md-7'>image carousel</div>
+      <div className='col-md-7'>
+        <Carousel showArrows={true} autoPlay={true} infiniteLoop={true}>
+          {images && images.map((i) => <img src={i.url} key={i.public_id} />)}
+        </Carousel>
+      </div>
       <div className='col-md-5'>
-        {JSON.stringify(images)}
         <Card
-          cover={
-            <img
-              className='p-1'
-              alt=''
-              // src={image}
-              style={{ hight: '150px', objectFit: 'cover' }}
-            />
-          }
           actions={[
             <Fragment>
               <ShoppingCartOutlined className='text-succsess' />
