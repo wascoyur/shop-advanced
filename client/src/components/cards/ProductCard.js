38e308c9c3bfa67fd5dd4019936203669794f175
wascoyur-xub <mytem20007@gmail.com>
2021-06-21 19:28:32 +0500

    124.Default Image 05:15

--- client/src/components/cards/ProductCard.js
+++ client/src/components/cards/ProductCard.js
@@ -1,45 +1,83 @@
 import { EyeOutlined, ShoppingCartOutlined } from '@ant-design/icons';
 import { Card } from 'antd';
 import React from 'react';
+import { Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import blank from '../../images/blank.png';
 
 const { Meta } = Card;
 
 const ProductCard = ({ product }) => {
   return (
-    // <div>{product.title}</div>
-    <Card
-      cover={
-        <img
-          className='p-1'
-          alt=''
-          src={product.images[0].url}
-          style={{ hight: '150px', objectFit: 'cover' }}
-        />
-      }
-      actions={[
-        <>
-          <ShoppingCartOutlined
-            key='delete'
-            /* className='text-warning' */
-            onClick={() => {}}
+    <Fragment>
+      {product.images && product.images.length ? (
+        <Card
+          cover={
+            <img
+              className='p-1'
+              alt=''
+              src={product.images[0].url}
+              style={{ hight: '150px', objectFit: 'cover' }}
+            />
+          }
+          actions={[
+            <>
+              <ShoppingCartOutlined
+                key='delete'
+                /* className='text-warning' */
+                onClick={() => {}}
+              />
+              <br />
+              Положить в корзину
+            </>,
+            <Link to={`/product/${product._id}`}>
+              <EyeOutlined key='edit' /*  className='text-danger' */ />
+              <br />
+              Подробнее..
+            </Link>,
+          ]}>
+          <Meta
+            title={product.title}
+            description={`${
+              product.description && product.description.substring(0, 10)
+            }...`}
           />
-          <br />
-          Положить в корзину
-        </>,
-        <Link to={`/product/${product._id}`}>
-          <EyeOutlined key='edit' /*  className='text-danger' */ />
-          <br />
-          Подробнее..
-        </Link>,
-      ]}>
-      <Meta
-        title={product.title}
-        description={`${
-          product.description && product.description.substring(0, 10)
-        }...`}
-      />
-    </Card>
+        </Card>
+      ) : (
+        <Card
+          cover={
+            <img
+              className='p-1'
+              alt=''
+              src={blank}
+              style={{ hight: '150px', objectFit: 'cover' }}
+            />
+          }
+          actions={[
+            <>
+              <ShoppingCartOutlined
+                key='delete'
+                /* className='text-warning' */
+                onClick={() => {}}
+              />
+              <br />
+              Положить в корзину
+            </>,
+            <Link to={`/product/${product._id}`}>
+              <EyeOutlined key='edit' /*  className='text-danger' */ />
+              <br />
+              Подробнее..
+            </Link>,
+          ]}>
+          <Meta
+            title={product.title}
+            description={`${
+              product.description && product.description.substring(0, 10)
+            }...`}
+          />
+        </Card>
+      )}
+    </Fragment>
   );
 };
 
--- client/src/components/cards/SingleProduct.js
+++ client/src/components/cards/SingleProduct.js
@@ -1,9 +1,10 @@
-import React, { Fragment } from 'react';
-import { Card } from 'antd';
-import { Link } from 'react-router-dom';
 import { HeartOutlined, ShoppingCartOutlined } from '@ant-design/icons';
+import { Card } from 'antd';
+import React, { Fragment } from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Link } from 'react-router-dom';
+import blank from '../../images/blank.png';
 
 const { Meta } = Card;
 
@@ -13,9 +14,21 @@ const SingleProduct = ({ product }) => {
   return (
     <Fragment>
       <div className='col-md-7'>
-        <Carousel showArrows={true} autoPlay={true} infiniteLoop={true}>
-          {images && images.map((i) => <img src={i.url} key={i.public_id} />)}
-        </Carousel>
+        {images && images.length ? (
+          <Carousel showArrows={true} autoPlay={true} infiniteLoop={true}>
+            {images && images.map((i) => <img src={i.url} key={i.public_id} />)}
+          </Carousel>
+        ) : (
+          <Card
+            cover={
+              <img
+                className='p-1'
+                alt=''
+                src={blank}
+                style={{ hight: '150px', objectFit: 'cover' }}
+              />
+            }></Card>
+        )}
       </div>
       <div className='col-md-5'>
         <Card
