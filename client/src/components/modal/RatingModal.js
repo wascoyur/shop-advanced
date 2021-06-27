170384e7f1c1c97e4af9a4ef6296c060580a7b27
wascoyur-xub <mytem20007@gmail.com>
2021-06-27 18:08:11 +0500

    130.Login to Leave Rating 03:51

--- client/src/components/modal/RatingModal.js
+++ client/src/components/modal/RatingModal.js
@@ -3,14 +3,25 @@ import { Modal, Button } from 'antd';
 import { toast } from 'react-toastify';
 import { useSelector } from 'react-redux';
 import { StarOutlined } from '@ant-design/icons';
+import { useHistory } from 'react-router';
 
 const RatingModal = ({ children }) => {
   const { user } = useSelector((state) => ({ ...state }));
   const [modalVisible, setModalVisible] = useState(false);
 
+  let history = useHistory()
+
+  const handleModal = () => {
+    if (user && user.token) {
+      setModalVisible(true)
+    } else {
+      history.push('/login')
+    }
+  }
+
   return (
     <Fragment>
-      <div onClick={() => setModalVisible(true)}>
+      <div onClick={() => handleModal()}>
         <StarOutlined className='text-danger' />
         <br /> {user ? 'Оставить отзыв' : 'Авторизуйтесь чтобы оставить отзыв. '}
       </div>
