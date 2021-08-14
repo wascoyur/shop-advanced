014af6b8e142f1b6fa9c8051cb6a78ae2063087d
HP-yur <mytem20007@gmail.com>
2021-08-14 15:55:31 +0500

    166.Redirect Back to Cart Page After Login 06:38

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -3,13 +3,15 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 const Cart = () => {
-  const { cart, user } = useSelector((state) => ({ ...state }))
-  const dispatch = useDispatch()
+  const { cart, user } = useSelector((state) => ({ ...state }));
+  const dispatch = useDispatch();
   const getTotal = () => {
     return cart.reduce((current, next) => {
-      return current + next.count *next.price
-    },0)
-  }
+      return current + next.count * next.price;
+    }, 0);
+  };
+
+  const saveOrderToDb = () => {};
 
   return (
     <div className='container-fluid pt-2'>
@@ -17,21 +19,46 @@ const Cart = () => {
         <h4>Корзина / {cart.length} товаров</h4>
         {/* {JSON.stringify(cart)} */}
       </div>
-      <div className="row">
-        <div className="col-md-8">
-          {!cart.length ? (<p>Корзина пуста.<Link to='/shop'>Продолжить покупки</Link></p>):('товары в корзине')}
+      <div className='row'>
+        <div className='col-md-8'>
+          {!cart.length ? (
+            <p>
+              Корзина пуста.<Link to='/shop'>Продолжить покупки</Link>
+            </p>
+          ) : (
+            'товары в корзине'
+          )}
         </div>
-        <div className="col-md-4">
+        <div className='col-md-4'>
           <h4>Суммарные расходы</h4>
           <hr />
           <p>Товары:</p>
           {cart.map((c, i) => {
-            return(<div key={i}><p>{c.title} х {c.count} шт. = {c.price * c.count} руб.</p></div>)
+            return (
+              <div key={i}>
+                <p>
+                  {c.title} х {c.count} шт. = {c.price * c.count} руб.
+                </p>
+              </div>
+            );
           })}
           <hr />
           Итого: <b>{getTotal()} руб</b>
           <hr />
-          {user?(<button className='btn btn-sm btn-primary mt-2'>Оплатить</button>):(<button className='btn btn-sm btn-primary mt-2'>Войти чтобы Оплатить</button>)}
+          {user ? (
+            <button
+              onClick={saveOrderToDb}
+              disabled={!cart.length}
+              className='btn btn-sm btn-primary mt-2'>
+              Оплатить
+            </button>
+          ) : (
+            <button className='btn btn-sm btn-primary mt-2'>
+              <Link to={{ pathname: '/login', state: { from: 'cart' } }}>
+                Войти чтобы Оплатить
+              </Link>
+            </button>
+          )}
         </div>
       </div>
     </div>
