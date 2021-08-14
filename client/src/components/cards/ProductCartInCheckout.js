ade1945e7f5dbd4426b09eb51c40e2fee41fb3b4
HP-yur <mytem20007@gmail.com>
2021-08-14 22:01:24 +0500

    168.React Modal Image 06:36

--- client/src/components/cards/ProductCartInCheckout.js
+++ client/src/components/cards/ProductCartInCheckout.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Fragment } from 'react';
+import ModalImage from 'react-modal-image';
+import blank from '../../images/blank.png'
 
 const ProductCartInCheckout = ({ p }) => {
   return (
     <Fragment>
       <tbody>
         <tr>
-          <td>Image</td>
+          <td>
+            <div style={{width:'50px', high:'auto'}}>
+              {p.images.length ? (
+                <ModalImage small={p.images[0].url} large={p.images[0].url} />
+              ) : (
+                <ModalImage small={blank} large={blank} />
+              )}
+            </div>
+          </td>
           <td>{p.title}</td>
           <td>{p.price}</td>
           <td>{p.brand}</td>
