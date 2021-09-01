1b8ffa96c50b7e2f96ab8724a317e2cd13a94f7d
HP-yur <mytem20007@gmail.com>
2021-09-01 17:01:03 +0500

    178.Get User Cart from Database 11:31

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -35,6 +35,16 @@ exports.userCart = async (req, res) => {
     orderdBy: user._id,
   }).save();
 
-  console.log('new cart saved', newCart);
+  // console.log('new cart saved', newCart);
   res.json({ ok: true });
+};
+
+exports.getUserCart = async (req, res) => {
+  const user = await User.findOne({ email: req.user.email }).exec();
+  let cart = await Cart.findOne({ orderdBy: user._id })
+    .populate('products.product', '_id title price totalAfterDiscount')
+    .exec();
+  const { products, cartTotal, totalAfterDiscount } = cart;
+
+  res.json({ products, cartTotal, totalAfterDiscount });
 };
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,16 +1,12 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-const  {authCheck}  = require('../middlewares/auth');
-const  {userCart}  = require('../controllers/user');
+const { authCheck } = require('../middlewares/auth');
+const { userCart, getUserCart } = require('../controllers/user');
 
 // console.log('routes',authCheck);
-router.post(
-  '/user/cart',
-  authCheck,
-  userCart,
-);
-
+router.post('/user/cart', authCheck, userCart);
+router.get('/user/cart', authCheck, getUserCart);
 
 // router.get("/user/cart", (req, res) => {
 //   res.json({
