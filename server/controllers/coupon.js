80dda7f93fedb6b66a67b000cc6e6e6d6057e8e7
HP-yur <mytem20007@gmail.com>
2021-09-02 23:23:45 +0500

    185.Coupon Routes and Controller Methods 07:13

---
+++ server/controllers/coupon.js
@@ -0,0 +1,24 @@
+const Coupon = require('../models/coupon');
+
+exports.create = async (req, res) => {
+  try {
+    const { name, expiry, discount } = req.body;
+    res.json(await new Coupon({ name, expiry, discount }).save());
+  } catch (error) {
+    console.log('err:', error);
+  }
+};
+exports.list = async (req, res) => {
+  try {
+    res.json(await Coupon.find({}).sort({ createdAt: -1 }).exec());
+  } catch (error) {
+    console.log('err:', error);
+  }
+};
+exports.remove = async (req, res) => {
+  try {
+    res.json(await Coupon.findByIdAndDelete(req.params.couponId).exec());
+  } catch (error) {
+    console.log('err:', error);
+  }
+};
