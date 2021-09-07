428b3922445cc3a6767a5185f9a55cafa8fd5ca7
HP-yur <mytem20007@gmail.com>
2021-09-07 21:32:02 +0500

    195.Stripe Setup Backend 14:55

--- client/src/pages/Payment.js
+++ client/src/pages/Payment.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+
+const promise = loadStripe(process.env.REACT_APPSTRIPE_KEY);
 
 const Payment = () => {
   return (
     <div>
       <div className='container p-5 text-center'>
-        <p>Завершите свою покупку</p>
+        <h4>Завершите свою покупку</h4>
+        <Elements stripe={promise}>
+          <div className='col-md-8 offset-md-2'>
+            <p>Проверка чекаут</p>
+          </div>
+        </Elements>
       </div>
     </div>
   );
