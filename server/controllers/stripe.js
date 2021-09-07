c0456da6fe14f2b0011ffad393a1c8df1c781412
HP-yur <mytem20007@gmail.com>
2021-09-07 20:01:30 +0500

    194.Stripe Payment Integration - Official Docs 14:06

---
+++ server/controllers/stripe.js
@@ -0,0 +1,13 @@
+const User = require('../models/user');
+const Cart = require('../models/cart');
+const Product = require('../models/product');
+const Coupon = require('../models/coupon');
+const stripe = require('stripe')(process.env.STRIPE_SECRET);
+
+exports.createPaymentIntent = async (req, res) => {
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: 100,
+    currency: 'usd',
+  });
+  res.send({ clientSecret: paymentIntent.client_secret });
+};
