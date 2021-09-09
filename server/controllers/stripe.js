151e98b5c6c214e062624b893c7d49a14693fa82
HP-yur <mytem20007@gmail.com>
2021-09-09 13:29:47 +0500

    198.Charging Actual Cart Total 09:50

--- server/controllers/stripe.js
+++ server/controllers/stripe.js
@@ -5,9 +5,15 @@ const Coupon = require('../models/coupon');
 const stripe = require('stripe')(process.env.STRIPE_SECRET);
 
 exports.createPaymentIntent = async (req, res) => {
+  const user = await User.findOne({ email: req.user.email }).exec();
+  const { cartTotal } = await Cart.findOne({ orderdBy: user._id }).exec();
+
+  console.log('cart total:', cartTotal);
+
   const paymentIntent = await stripe.paymentIntents.create({
-    amount: 100,
+    amount: cartTotal * 100,
     currency: 'usd',
   });
+
   res.send({ clientSecret: paymentIntent.client_secret });
 };
