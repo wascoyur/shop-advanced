192d5c6927aec763be8135c90c4720e08e67c159
HP-yur <mytem20007@gmail.com>
2021-09-09 12:28:44 +0500

    197.Successful Payment 13:52

--- client/src/components/StripeCheckout.js
+++ client/src/components/StripeCheckout.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
 import { useSelector, useDispatch } from 'react-redux';
 import { createPaymentIntent } from '../functions/stripe';
+import { Link } from 'react-router-dom';
 
 const StripeCheckout = ({ history }) => {
   const dispatch = useDispatch();
@@ -22,8 +23,31 @@ const StripeCheckout = ({ history }) => {
     });
   }, []);
 
-  const handleSubmit = async (e) => {};
-  const handleChange = async (e) => {};
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setProcessing(true);
+
+    const payload = await stripe.confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: e.target.name.value,
+        },
+      },
+    });
+    if (payload.error) {
+      setError(`При оплате произошла ошибка: ${payload.error.message}`);
+      setProcessing(false);
+    } else {
+      setError(null);
+      setProcessing(false);
+      setSuccsed(true);
+    }
+  };
+  const handleChange = async (e) => {
+    setDisabled(e.empty);
+    setError(e.error);
+  };
   const cartStyle = {
     style: {
       base: {
@@ -43,24 +67,35 @@ const StripeCheckout = ({ history }) => {
   };
   return (
     <>
+      <p className={succsed ? 'result-message' : 'result-message-hidden'}>
+        Оплата успешна{' '}
+        <Link to={'user/history'}>Посмотреть в истории покупок </Link>
+      </p>
       <form className='stripe-form' id='payment-form' onSubmit={handleSubmit}>
         <CardElement
           id='card-element'
           options={cartStyle}
           onChange={handleChange}
         />
+
+        <button
+          className='stripe-button'
+          disabled={processing || disabled || succsed}>
+          <span id='button-text'>
+            {processing ? (
+              <div className='spinner' id='spinner'></div>
+            ) : (
+              'Оплатить'
+            )}
+          </span>
+        </button>
+        <br />
+        {error && (
+          <div className='card-error' role='alert'>
+            {error.message}
+          </div>
+        )}
       </form>
-      <button
-        className='stripe-button'
-        disabled={processing || disabled || succsed}>
-        <span id='button-text'>
-          {processing ? (
-            <div className='spinner' id='spinner'></div>
-          ) : (
-            'Оплатить'
-          )}
-        </span>
-      </button>
     </>
   );
 };
