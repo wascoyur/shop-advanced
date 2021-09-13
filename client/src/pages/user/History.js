7f5c057ac8f5285f192bdd44de3526ac3930f18a
HP-yur <mytem20007@gmail.com>
2021-09-13 13:21:46 +0500

    209.Purchase Orders in Card 06:15

--- client/src/pages/user/History.js
+++ client/src/pages/user/History.js
@@ -9,21 +9,44 @@ const History = () => {
 
   const loadUserOrders = () => {
     getUserOrders(user.token).then((res) => {
-      console.log('orders:', JSON.stringify(res.data, null, 4));
+      // console.log('orders:', JSON.stringify(res.data, null, 4));
       setOrders(res.data);
     });
   };
   useEffect(() => {
     loadUserOrders();
   }, []);
+
+  const showOrderInTable = (order) => <p>товары </p>;
+
+  const showEachOrders = () =>
+    orders.map((order, i) => (
+      <div key={i} className='m-5 p-3 card'>
+        <p>show payment info</p>
+        {showOrderInTable(order)}
+        <div className='row'>
+          <div className='col'>
+            <p>PDF download</p>
+          </div>
+        </div>
+      </div>
+    ));
+
   return (
     <div className='container-fluid'>
       <div className='row'>
         <div className='col-md-2'>
           <UserNav />
         </div>
-        <div className='col'>user History page</div>
-        {orders.length}
+
+        <div className='col text-center'>
+          <h4 className='col '>
+            {orders.length
+              ? 'Ваши оплаченные покупки'
+              : 'Нет оплаченных покупок'}
+          </h4>
+          {showEachOrders()}
+        </div>
       </div>
     </div>
   );
