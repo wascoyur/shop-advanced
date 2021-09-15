e5ae282b1532af8784cf54c410067d143f1b78d8
HP-yur <mytem20007@gmail.com>
2021-09-15 23:21:41 +0500

    216.Load All Orders in Admin Dashboard 13:17

--- client/src/pages/admin/AdminDashboard.js
+++ client/src/pages/admin/AdminDashboard.js
@@ -1,8 +1,30 @@
 import { Col } from 'antd';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import AdminNav from '../../components/nav/AdminNav';
+import { getOrders, changeStatus } from '../../functions/admin';
+import { useSelector, useDispatch } from 'react-redux';
+import { toast } from 'react-toastify';
 
 const AdminDashboard = () => {
+  const [orders, setOrders] = useState([]);
+  const { user } = useSelector((state) => ({ ...state }));
+
+  useEffect(() => {
+    loadOrders();
+  }, []);
+
+  const loadOrders = async () => {
+    await getOrders(user.token).then((res) => {
+      // console.log('adminDash res:', JSON.stringify(res, null, 4));
+      setOrders(res.data);
+    });
+  };
+  const handleStatusChange = (orderId, orderStatus) => {
+    changeStatus(user.token).then((res) => {
+      toast.success('Статус заказа обновлен.');
+      loadOrders();
+    });
+  };
   return (
     <div className='container-fluid'>
       <div className='row'>
@@ -12,6 +34,7 @@ const AdminDashboard = () => {
 
         <Col>
           <h4>Панель администратора</h4>
+          {JSON.stringify(orders)}
         </Col>
       </div>
     </div>
