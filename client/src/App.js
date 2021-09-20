d5741d1a229d15194c8c48c54b2fe44fa44de3cf
HP-yur <mytem20007@gmail.com>
2021-09-20 17:42:27 +0500

    remove text

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, Fragment } from 'react';
+import React, { useEffect, Fragment, lazy } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
